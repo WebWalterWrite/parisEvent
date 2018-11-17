4cf6b5633c8cab65593820859015fcb58b8176a8
WalterWhiteGit <walterwhitegithub@gmail.com>
2018-11-18 00:17:25 +0100

    no changes

--- src/hoc/events.hoc.js
+++ src/hoc/events.hoc.js
@@ -1,34 +1,41 @@
 import React, { Component } from "react";
-import PropTypes from 'prop-types';
-import Loader from '../utils/loader';
+import PropTypes from "prop-types";
+import Loader from "../utils/loader";
 
-
-const Hoc = (WrappedComponent, action ) => {
-
+const Hoc = (WrappedComponent, action) => {
 	class EventsHoc extends Component {
-	
 		constructor(props) {
 			super(props);
-			console.log(props)
+			console.log(props);
 			this.state = {
 				data: []
 			};
 		}
 
-		componentDidUpdate = async (prevProps) => {
-			// Typical usage (don't forget to compare props):
-			console.log(prevProps);
-			if (this.props.match.params.name !== prevProps.match.params.name) {
-				const { name } = this.props.match.params
-				const EventsList = await action(name);
-				this.insertEvents(EventsList.data);
+		componentDidUpdate = async prevProps => {
+			try {
+				if (this.props.match.params.name !== prevProps.match.params.name) {
+					const { name } = this.props.match.params;
+					const EventsList = await action(name);
+					const { data, status, message } = EventsList;
+					status === "success" ? this.insertEvents(data) : console.log(message);
+				}
+			} catch (e) {
+				console.log(e.message);
 			}
-		  }
-		
-		componentDidMount= async () => {
-			const { name } = this.props.match.params
-			const EventsList = await action(name);
-			this.insertEvents(EventsList.data);
+		};
+
+		componentDidMount = async () => {
+			try {
+				const { name } = this.props.match.params;
+				console.log(name);
+				const EventsList = await action(name);
+				const { data, status, message } = EventsList;
+				console.log(data, status);
+				status === "success" ? this.insertEvents(data) : console.log(message);
+			} catch (e) {
+				console.log(e.message);
+			}
 		};
 
 		insertEvents = list => {
@@ -36,21 +43,22 @@ const Hoc = (WrappedComponent, action ) => {
 		};
 
 		render() {
-			if(!this.state.data || this.state.data.length === 0)
-			return <Loader event = {this.props.match.params.name} />;
-			return <WrappedComponent events={this.state.data} event={this.props.match.params.name}/>;
-		
+			if (!this.state.data || this.state.data.length === 0)
+				return <Loader event={this.props.match.params.name} />;
+			return (
+				<WrappedComponent
+					events={this.state.data}
+					event={this.props.match.params.name}
+				/>
+			);
 		}
-    };
+	}
 
 	return EventsHoc;
-	
-	
 };
 
 Hoc.propTypes = {
-	action : PropTypes.bool.isRequired
-}
-
+	action: PropTypes.bool.isRequired
+};
 
 export default Hoc;
