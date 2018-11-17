07295b5388f0bc96dbe287b9904f2b66a13e5fa8
WalterWhiteGit <walterwhitegithub@gmail.com>
2018-11-18 00:19:00 +0100

    type:refactor desc: add reponsive style et obj data

--- src/hoc/nav.hoc.js
+++ src/hoc/nav.hoc.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
-
 import styled from 'styled-components';
+
+// import styles
 import {media} from '../styles/responsive.styled';
+
 
 const Nav = styled.nav`
     width:100%;
-    padding:10px;
+
     color:white;
     ${media.desktop`
+    padding:10px;
     ul{
         display: flex;
         justify-content: space-around;
     };
     `}
     ${media.desktopL`
+    padding:10px;
     ul{
         display: flex;
         justify-content: space-around;
@@ -22,15 +26,27 @@ const Nav = styled.nav`
     `}
 
      ${media.iphonePlus`
+     padding:0px;
     ul{
-        display: block;
-        transform: translate(-500px,0);
+        display: flex;
     };
     `}
     li{ list-style-type:none;}
+`
 
+const Links = styled(Link)`
+    ${media.desktop`
+        display:none;
+    `}
+    ${media.iphonePlus`
+        font-size:10px;
+        display:flex; 
+        justify-content:space-around;
+        div{width:70%; text-align:center;}
+        img{ width:100%;}
+    `}
 
-
+    
 `
 
 // retourne un menu 
@@ -40,7 +56,13 @@ export const Mapper = (props) => {
             <ul>
             {props.data.map((elem, i) =>{
                 return (
-                    <li key={i}><Link to={`/parisbyme/evenement/${elem.name}`}>{elem.name}</Link><hr/></li>
+                    <li key={i}>
+                    <Links to={`/parisbyme/evenement/${elem.name}`}>
+                        <div>
+                            <img src={elem.img} alt=""/>
+                            <p>{elem.shortname}</p>
+                        </div>
+                    </Links><hr/></li>
                 )
             })}
             </ul>
