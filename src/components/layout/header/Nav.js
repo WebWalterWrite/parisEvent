edfed773a8a23a83c52996f291c59f5557485826
WalterWhiteGit <walterwhitegithub@gmail.com>
2018-11-18 00:16:26 +0100

    type:refactor desc: ajout proprietes img, shortname à obj nav

--- src/components/layout/header/Nav.js
+++ src/components/layout/header/Nav.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import {Mapper} from '../../../hoc/nav.hoc';
+import theater from '../../../images/theater.png';
+import expo from '../../../images/expo.png';
+import humour from '../../../images/comedy.png';
+import bx from '../../../images/beaux_arts.png';
+import artco from '../../../images/arct_co.png';
 
 
 const nav = [
-    {name:'Expositions'},
-    {name: 'Humour'},
-    {name: 'Théâtre' },
-    {name: 'Art Contemporain'},
-    {name: "Beaux-Arts"}
+    {name:'Expositions',shortname:'Expo', img:expo},
+    {name: 'Humour', shortname:'Humour', img:humour},
+    {name: 'Théâtre', shortname:'Théâtre', img:theater },
+    {name: 'Art Contemporain', shortname:"Art-Cont",img:artco},
+    {name: "Beaux-Arts",shortname:'Bx-Arts', img:bx}
 ]
 
 const Nav = () => (   
